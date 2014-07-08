b39d1f28a3f863e29cbd78ddbb5bfb5d8dab2578
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-07-08 11:51:44 +0100

    Adding docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
 class DockingStation
+	DEFAULT_CAPACITY = 10
 
-	def initialize
+	def initialize(options = {})
 		@bikes = []
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
 	end
 
 	def bikes
@@ -11,6 +13,30 @@ class DockingStation
 	def dock(bike)
 		bikes << bike
 
+	end
+
+	def empty?
+		bikes.empty?
+	end
+
+	def bike_count
+		bikes.count
+	end
+
+	def release(bike)
+		bikes.delete(bike)
+	end
+
+	def full?
+		capacity == bike_count
+	end
+
+	def capacity
+		@capacity
+	end
+
+	def available_bikes
+		bikes.reject { |bike| bike.broken? }
 	end
 
 end
