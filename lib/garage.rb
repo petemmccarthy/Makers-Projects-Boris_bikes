461a1ed8db56ccff036a15963ea819f6091c1c5e
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-07-09 16:58:26 +0100

    Added more garage functionality

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,7 +1,12 @@
 require_relative 'bike_container'
+require 'bike'
 
 class Garage
 
 	include BikeContainer
 
+	def receives_only_broken_bikes(bike)
+		bikes.select { |bike| bike.broken? }
+		dock(bike)
+	end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -3,24 +3,44 @@ require 'bike'
 
 describe Garage do
 
-it 'has no bikes when created' do
-	depot = Garage.new
-	expect(depot.bike_count).to eq (0)
-end
+	let(:depot) {Garage.new}
+	let(:trek) {Bike.new}
+	let(:broken_bike) {Bike.new.break!}
+
+	it 'has no bikes when created' do
+		expect(depot.bike_count).to eq (0)
+	end
+
+	it 'can receive bikes' do
+		expect(depot.bike_count).to eq (0)
+		depot.dock(broken_bike)
+		expect(depot.bike_count).to eq (1)
+	end
+
+	it 'can receive only broken bikes' do
+		expect(depot.bike_count).to eq (0)
+		depot.receives_only_broken_bikes(broken_bike)
+		# depot.receives_only_broken_bikes(trek) 
+		expect(depot).to include(broken_bike)
+	end
+
+
+
+	# it 'can release bikes' do
+	# 	expect(depot.bike_count).to eq (0)
+	# 	depot.dock(broken_bike)
+	# 	expect(depot.bike_count).to eq (1)
+	# end
 
 # it 'can fix a broken bike' do
-# 	depot = Garage.new
-# 	broken_bike = Bike.new.break!
-# 	# expect(depo.bikes).to eq [broken_bike]
-# 	# # broken_bike.fix
-# 	# # expect(garage.bikes)
-
+# 	trek = Bike.new
+# 	expect(depot.bikes).to eq [broken_bike]
 # end
 
-it 'knows if the bike is broken or not' do 
-	depot = Garage.new
-	expect(Bike.new.broken?).to eq (broken)
-end
 
+# it 'knows if the bike is broken or not' do 
+# 	depot = Garage.new
+# 	expect(Bike.new.broken?).to eq (broken)
+# end
 
 end
